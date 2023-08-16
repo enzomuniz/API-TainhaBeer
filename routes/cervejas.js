65619381c831a96d35dba78d73bf33a62ec65d4c
enzomuniz <99302958+enzomuniz@users.noreply.github.com>
2023-08-15 21:49:37 -0300

    Update cervejas.js

--- routes/cervejas.js
+++ routes/cervejas.js
@@ -7,6 +7,9 @@ router.get('/nome/:nome', controller.buscaNome);
 router.get('/origem/:nacionalidade', controller.buscaOrigem);
 router.get('/tipo/:tipo', controller.buscaTipo);
 router.get('/ordenar', controller.ordenar);
+router.post('/postar', controller.postar);
+router.put('/atualizar/:id' , controller.put);
+router.delete('/deletar/:id' , controller.deletar);
 
 
 module.exports = router;
